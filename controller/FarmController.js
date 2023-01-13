d6ef60fc11870335dbc30c5feb0a8ab092038986
Fahama Khan <fahamakhan990@gmail.com>
2023-01-14 03:40:48 +0500

    new endpoint getFarmById

--- controller/FarmController.js
+++ controller/FarmController.js
@@ -1,12 +1,12 @@
 // controllers/farmController.js
-const Farm = require('../models/Farm');
+const Farm = require("../models/Farm");
 
 // Create a new Farm
 exports.create_farm = async (req, res) => {
   const farm = new Farm({
     farm_name: req.body?.farm_name,
     farm_address: req.body?.farm_address,
-    role: req.body?.farm_name
+    role: req.body?.farm_name,
   });
   try {
     await farm.save();
@@ -26,4 +26,13 @@ exports.get_farms = async (req, res) => {
   }
 };
 
-
+//Get Farm By Id
+exports.getFarmById = async (req, res) => {
+  try {
+    const farm = await Farm.findById(req.params.id);
+    if(!farm) return res.status(404).send("Farm not found");
+    res.send(farm);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
