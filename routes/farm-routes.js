d05ccc7c9d90b1cdce8a9676d9e7f2463e71f2f3
Fahama Khan <fahamakhan990@gmail.com>
2023-01-14 03:41:14 +0500

    add new route for get farm by id

--- routes/farm-routes.js
+++ routes/farm-routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const FarmController = require('../controller/FarmController');
 
 router.post('/', FarmController.create_farm);
-
-
 router.get('/', FarmController.get_farms);
-
+router.get('/:id', FarmController.getFarmById);
 module.exports = router;
