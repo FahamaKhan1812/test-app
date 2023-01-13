0c15012e0daff4746431c4d753ceb1d928cfbc6a
Fahama Khan <fahamakhan990@gmail.com>
2023-01-14 03:42:37 +0500

    register new route for getFarmById

--- app.js
+++ app.js
@@ -4,28 +4,33 @@ const mogoonse = require("mongoose");
 
 //Routes
 const superAdminRoutes = require("./routes/super-admin-routes");
-const FarmRoutes=require("./routes/farm-routes");
-const UserRoutes=require("./routes/user-routes");
+const FarmRoutes = require("./routes/farm-routes");
+const UserRoutes = require("./routes/user-routes");
 
 //connect to mongodb server
-const dbURI = "mongodb+srv://fahama:YviAIPJlQUwbmwW1@final-year-project.3wxb73f.mongodb.net/FYP"
- const app = express();
+const dbURI =
+  "mongodb+srv://fahama:YviAIPJlQUwbmwW1@final-year-project.3wxb73f.mongodb.net/FYP";
+const app = express();
 
 // For Super Admin
 app.use(express.json()); // this line is for parsing json body
-app.use('/createnewadmin', superAdminRoutes);
+app.use("/createnewadmin", superAdminRoutes);
 
+app.use("/createfarm", FarmRoutes);
+app.use("/getfarms", FarmRoutes);
+app.use('/getfarmid', FarmRoutes);
 
-app.use('/createfarm', FarmRoutes);
-app.use('/getfarms', FarmRoutes);
-
-app.use('/createuser', UserRoutes)
-
-
-
+app.use("/createuser", UserRoutes);
 mogoonse
-  .connect(dbURI)
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(5000, () => console.log("Conneted!!"));
   })
-  .catch((err) => console.log(err)) ;
+  .catch((err) => console.log(err));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if(req.accepts('json')){
+    res.send({error: "40404 Not Found"});
+  }
+});
