ab9b16f339516679533fe0f9297cee144cf2f148
Fahama Khan <fahamakhan990@gmail.com>
2023-01-15 01:55:31 +0500

    update the user schema

--- models/User.js
+++ models/User.js
@@ -2,24 +2,31 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["superadmin", "farmowner"],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  farm: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "farms",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
