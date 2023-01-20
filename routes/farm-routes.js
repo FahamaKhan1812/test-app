c06e77b821d0da3561c93b8d8d04ed6fc3392bfc
Fahama Khan <fahamakhan990@gmail.com>
2023-01-20 22:54:33 +0500

    update routes

--- routes/farm-routes.js
+++ routes/farm-routes.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const FarmController = require('../controller/FarmController');
 
-router.post('/createnewfarm', FarmController.create_farm);
-router.get('/getallfarms', FarmController.get_farms);
-router.get('/getfarmbyid:id', FarmController.getFarmById);
+const FarmController = require("../controller/FarmController");
+const { userAuth, checkRole } = require("../utils/Auth");
+
+router.post("/createnewfarm", FarmController.create_farm);
+router.get(
+  "/getallfarms",
+  userAuth,
+  checkRole(["superadmin", "farmowner"]),
+  FarmController.get_farms
+);
+router.get("/getfarmbyid:id", FarmController.getFarmById);
+router.get("/gettesting", FarmController.getAnimalByFarm_uuid);
+
 module.exports = router;
