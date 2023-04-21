f2d258b94fa70553caa41e9856bfc2145f90b87c
Fahama Khan <fahamakhan990@gmail.com>
2023-04-21 06:55:17 +0500

    add new methods

--- controller/User.js
+++ controller/User.js
@@ -1,5 +1,6 @@
 // controllers/userController.js
-const User = require("../models/user");
+const User = require("../models/User");
+const sterilizeUsersData = require("../utils/helper/sterilize.user.response");
 
 exports.create_user = async (req, res) => {
   const user = new User({
@@ -13,5 +14,60 @@ exports.create_user = async (req, res) => {
     res.send(user);
   } catch (err) {
     res.status(400).send(err);
+  }
+};
+
+exports.get_all_users = async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users) {
+      const m = await sterilizeUsersData(users);
+
+      return res.status(200).json({
+        success: true,
+        message: [],
+        data: m,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: ["Server error try again"],
+      error: err,
+    });
+  }
+};
+
+// Get User by their Ids
+exports.getUserById = async (req, res) => {
+  const { farm_Id, slaughter_house_Id } = req.query;
+  let filter = {};
+
+  if (farm_Id) {
+    filter.farm_Id = farm_Id;
+  }
+
+  if (slaughter_house_Id) {
+    filter.slaughter_house_Id = slaughter_house_Id;
+  }
+
+  try {
+    const users = await User.find(filter);
+
+    if (users.length === 0) {
+      return res.status(200).json({
+        success: false,
+        message: "No users found.",
+        data: []
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: [],
+        data: await sterilizeUsersData(users),
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
