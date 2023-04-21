6f0a021626a90d19401b8178b855463674a9ae82
Fahama Khan <fahamakhan990@gmail.com>
2023-04-21 06:57:45 +0500

    add new method for Sterilize Users Data

---
+++ utils/helper/sterilize.user.response.js
@@ -0,0 +1,20 @@
+async function sterilizeUsersData(arr) {
+  if (arr) {
+    const newArr = [];
+    arr.map((obj) => {
+      const newCont = {
+        _id: obj?._id,
+        
+        name: obj?.name,
+        email: obj?.email,
+        role: obj?.role,
+        username: obj?.username,
+        createdAt: obj?.createdAt,
+      };
+      newArr.push(newCont);
+    });
+    return newArr;
+  }
+}
+
+module.exports = sterilizeUsersData;
