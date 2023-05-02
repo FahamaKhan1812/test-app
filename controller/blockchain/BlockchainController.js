00d33d2d598f1bd0e8264325fdf45e62df39dda3
Dawar Shamim <DawarShamim1@gmail.com>
2023-05-02 12:12:02 +0500

    Endpoints for BlockChain

--- controller/blockchain/BlockchainController.js
+++ controller/blockchain/BlockchainController.js
@@ -1,7 +1,6 @@
-const { ethers ,utils } = require('ethers');
 const { AlchemyProvider } = require('@ethersproject/providers');
+const { ethers } = require('hardhat');
 
-// interact.js
 API_URL = "https://polygon-mumbai.g.alchemy.com/v2/wQervsrXuCr31pfJlVI9CUjh1zXElRWu"
 API_KEY = "wQervsrXuCr31pfJlVI9CUjh1zXElRWu"
 PRIVATE_KEY = "d24c8db45a844cf054c3a02996631e6608bc6723e9f6ce56f0754c861aa98858"
@@ -64,82 +63,92 @@ exports.retrieveDataBlockchain= async (req,res) => {
               });
             }
           };
-    
-
-          
-exports.createProduct= async(req,res)=> {
-    const ID=req.body?.ProductID;
-    const Details=req.body?.ProductDetails;
-    const Dates=req.body?.SnEDate;
+              
+exports.createProduct= async(req,res)=>{
+    const ID = req.body?.ProductID;
+    const Details = req.body?.ProductDetails;
+    const Dates = req.body?.SnEDate;
     try {
-        const netObj = {name: 'maticmum',
-          chainId: 80001  // hardwired
-        }
-          const provider = new AlchemyProvider(netObj, API_KEY);
-          const signer = new ethers.Wallet(PRIVATE_KEY, provider);
-          const MeatContract = new ethers.Contract(CONTRACT_ADDRESS,contract.abi, signer);
-          console.log("here");
-          const createTx = await MeatContract.create("test1","Details", "Dates",{gasPrice: 100000000, gasLimit: 10000000});
-          console.log("there");
-          const CreateRep = await createTx.wait();
-          console.log("create() function executed successfully with ID test1")
-          console.log(CreateRep);
-          const Output ={"ID":ID,"Details":Details,"Dates":Dates};
-        console.log(Output);
+      const network = 'maticmum';
+      const provider = new ethers.providers.AlchemyProvider(network, API_KEY);
+      const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+      const MeatContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+      const createTx = await MeatContract.create(ID, Details, Dates, {
+        gasPrice: ethers.utils.parseUnits('100', 'gwei'),
+        gasLimit: 10000000 });
+      await createTx.wait();
+      const Output = { ID, Details, Dates };  
+      return res.status(200).json({
+        success: true,
+        message: [],
+        Output
+      });
+    } catch (err) {
+      console.log(err);
+      let errorMessage = err?.reason;
+      return res.status(400).json({
+        success: false,
+        message: [err, errorMessage]
+      });
+    }  };
 
-        return res.status(200).json({
-            success: true,
-            message: [],
-            Output,
-          });
-        } catch (err) {
-            console.log(err);
-          let errorMessage = err?.reason;
-          return res.status(400).json({
-            success: false,
-            message: [err,errorMessage],
-          });
-        }
-      };
-  //   updatedistributorinBlockchain(){};
-//   updatedistributorinBlockchain(){};
 
-    async function main() {
-      try {
-        const netObj = {
-          name: 'maticmum',
-          chainId: 80001  // hardwired
-        }
-        const provider = new ethers.providers.AlchemyProvider(netObj, API_KEY);
-        
-        const new_network = await provider.getNetwork();
-        console.log("Connected to network:", new_network.name);
-            
-            const signer = new ethers.Wallet(PRIVATE_KEY, provider);
-            
-            // Contract
-            const MeatContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
-    
-          // Call the create() function
-          // const createTx = await MeatContract.create("64500cbec5dc2082386dd57c","63d2a2a98a0027d2d7e51de7:63d2ed0515f1ab6980906e89:63d3040cacba6534e1222af4:644f78290a63f85f9ed43047", "2023-05-02:2023-09-02", { gasLimit: 300000 });
-          // const CreateRep = await createTx.wait();
-          // console.log("create() function executed successfully with ID 64500cbec5dc2082386dd57c", CreateRep);
+exports.updatedistributorinBlockchain = async(req,res)=>{ 
+    const ID = req.body?.ProductID;
+    const DistributorID = req.body?.distributorid;
+    try {
+      const network = 'maticmum';
+      const provider = new ethers.providers.AlchemyProvider(network, API_KEY);
+      const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+      const MeatContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+
+      const updateDistributorTx = await MeatContract.UpdateDistributor(ID, DistributorID, {
+        gasPrice: ethers.utils.parseUnits('100', 'gwei'),
+        gasLimit: 10000000 });
+     await updateDistributorTx.wait();
       
-          // // Call the updateDistributor() function
-          // const updateDistributorTx = await MeatContract.UpdateDistributor("64500cbec5dc2082386dd57c", "63c1d7a9027da84b46f15286");
-          // const DistributorRep =  await updateDistributorTx.wait();
-          // console.log("UpdateDistributor() function executed successfully ",DistributorRep);
-          // console.log("Update Distributor of Product ID 64500cbec5dc2082386dd57c with 63c1d7a9027da84b46f15286");
-          
+      const Output = { ID, DistributorID};  
+      return res.status(200).json({
+        success: true,
+        message: [],
+        Output
+      });
+    } catch (err) {
+      console.log(err);
+      let errorMessage = err?.reason;
+      return res.status(400).json({
+        success: false,
+        message: [err, errorMessage]
+      });
+    }  };
+
+
+exports.updateretailorinBlockchain=async (req,res)=>{
+    const ID = req.body?.ProductID;
+    const RetailorID = req.body?.retailorid;
+    try {
+      const network = 'maticmum';
+      const provider = new ethers.providers.AlchemyProvider(network, API_KEY);
+      const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+      const MeatContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+      const updateRetailorTx = await MeatContract.UpdateRetailor(ID, RetailorID, {
+          gasPrice: ethers.utils.parseUnits('100', 'gwei'),
+          gasLimit: 10000000 });
+        await updateRetailorTx.wait();
       
-          // // Call the updateRetailor() function
-          // const updateRetailorTx = await MeatContract.UpdateRetailor("64500cbec5dc2082386dd57c", "63c1da687a1519b2b0117de7");
-          // const RetailorRep = await updateRetailorTx.wait();
-          // console.log("UpdateRetailor() function executed successfully ",RetailorRep);
-          // console.log("Update Retailor of Product ID 64500cbec5dc2082386dd57c with 63c1da687a1519b2b0117de7");
-          
-          // Call the retrieve() function
-        } catch (error) {
-          console.error(error);
-        }
-      };
+      const Output = { ID, RetailorID};  
+      return res.status(200).json({
+        success: true,
+        message: [],
+        Output
+      });
+    } catch (err) {
+      console.log(err);
+      let errorMessage = err?.reason;
+      return res.status(400).json({
+        success: false,
+        message: [err, errorMessage]
+      });
+    }  };
--- routes/blockchain-routes.js
+++ routes/blockchain-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const ProductInfoController = require('../controller/blockchain/product-dataController');
 const BlockchainController = require('../controller/blockchain/BlockchainController');
-router.get("/getproductbyid/:id", ProductInfoController.getDataFromBlockchain);
 
 router.post("/CreateProductOnBlockchain",BlockchainController.createProduct);
 router.get("/GetProductFromBlockchain/:id",BlockchainController.retrieveDataBlockchain);
+router.put("/UpdateDistributor",BlockchainController.updatedistributorinBlockchain);
+router.put("/UpdateRetailor",BlockchainController.updateretailorinBlockchain);
+
+
 module.exports = router;
