74dd11c565250903e14a5c2a1437c6f91c06cb15
Dawar Shamim <DawarShamim1@gmail.com>
2023-05-07 22:02:31 +0500

    added roles

--- routes/distributor-routes.js
+++ routes/distributor-routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const DistributorController = require('../controller/DistributorController');
-
+const { userAuth, checkRole } = require("../utils/Auth");
 router.post('/createnewdistributor', DistributorController.create_distributor);
-router.get('/getalldistributors', DistributorController.get_distributors);
+router.get(
+    '/getalldistributors',
+    userAuth,
+    checkRole(["superadmin"]),
+    DistributorController.get_distributors
+  );
+
 router.get('/getdistributorbyid:id', DistributorController.getdistributorById);
 
 module.exports = router;
