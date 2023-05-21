fb8c32736438c24e70ac39c6d24806745ecddad4
Fahama Khan <fahamakhan990@gmail.com>
2023-05-21 12:48:14 +0500

    fix issue

--- utils/Auth.js
+++ utils/Auth.js
@@ -49,45 +49,53 @@ const userRegister = async (userDetails, role, res) => {
 
 //Login user
 const userLogin = async (userCreds, res) => {
-  let { username, password } = userCreds;
-  // First Check if the username is in the database
-  const user = await User.findOne({ username });
-  if (!user) {
-    return res.status(404).json({
-      message: "Username is not found.",
-      success: false,
-    });
-  }
+  try {
+    let { username, password } = userCreds;
+    // First Check if the username is in the database
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+        success: false,
+      });
+    }
 
-  //Now check for the password
-  let isMatch = await bcrypt.compare(password, user.password);
-  if (isMatch) {
-    // create JWT Token for the user
-    let token = jwt.sign(
-      {
-        user_id: user._id,
-        role: user.role,
+    //Now check for the password
+    let isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      // create JWT Token for the user
+      let token = jwt.sign(
+        {
+          user_id: user._id,
+          role: user.role,
+          username: user.username,
+          name: user.name,
+          farm_Id: user?.farm_Id,
+          slaughter_house_Id: user?.slaughter_house_Id,
+        },
+        "SUPER-SECRETKEY",
+      );
+      let result = {
         username: user.username,
-        name: user.name,
-        farm_Id: user?.farm_Id,
-        slaughter_house_Id: user?.slaughter_house_Id,
-      },
-      "SUPER-SECRETKEY",
-    );
-    let result = {
-      username: user.username,
-      token,
-    };
-    return res.status(200).json({
-      message: "Login Successfully",
-      success: true,
-      data: result
-    });
-  } else {
-    return res.status(403).json({
-      //403 = Unauthorized
-      message: "Incorrect password",
+        token,
+      };
+      return res.status(200).json({
+        message: "Login Successfully",
+        success: true,
+        data: result,
+      });
+    } else {
+      return res.status(401).json({
+        //403 = Unauthorized
+        message: "Invalid credentials",
+        success: false,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
       success: false,
+      message: "Something went wrong. Please try again",
+      error: error?.message,
     });
   }
 };
@@ -121,4 +129,5 @@ module.exports = {
   checkRole,
   userRegister,
   userLogin,
+  resetUserPassword,
 };
