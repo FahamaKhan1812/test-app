175908c418cd521e46f82cc02d64f80e7dc18595
Fahama Khan <fahamakhan990@gmail.com>
2023-05-23 00:08:05 +0500

    add new function

---
+++ utils/helper/time-frame-selection.js
@@ -0,0 +1,17 @@
+//Time frame Selection:
+export function getDateRangeOffset(dateRange) {
+  switch (dateRange) {
+    case "1week":
+      return 7;
+    case "1month":
+      return 30;
+    case "3months":
+      return 90;
+    case "6months":
+      return 180;
+    case "1year":
+      return 365;
+    default:
+      return 7;
+  }
+}
